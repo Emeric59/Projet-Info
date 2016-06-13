3e6fcb5cca4b11ae40b4904b6faba37340e761fb
flo <docte@MSI-FLO>
2016-06-13 15:52:21 +0200

    VolumeControl

---
+++ RemoteWeb/RemoteWeb/scripts/Volume.js
@@ -0,0 +1,24 @@
+﻿var constellation = $.signalR.createConstellationConsumer("http://localhost:8088", "615bd655bc724bc2c8eccf001f0aaf7df557849b", "RemoteControl")
+
+constellation.connection.stateChanged(function (change) {
+    if (change.newState === $.signalR.connectionState.connected) {
+        $("#state").text("Connecté")
+    } else {
+        $("#state").text("Non connecté")
+    }
+});
+
+$("#Mute").click(function () {
+    constellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", "mute");
+});
+
+$("#IncreaseVolume").click(function () {
+    constellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", "plus");
+});
+
+$("#DecreaseVolume").click(function () {
+    constellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", "moins");
+});
+
+constellation.connection.start();
+
