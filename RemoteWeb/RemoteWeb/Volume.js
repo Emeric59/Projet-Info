70387b084eafa2d00b9ef218f30187b374dfcca6
Pierre <Pierre@PCDEPIERRE>
2016-06-13 15:37:13 +0200

    Volume.js

---
+++ RemoteWeb/RemoteWeb/Volume.js
@@ -0,0 +1,12 @@
+﻿$("#Mute").click(function () {
+    cconstellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", {"mute"});
+});
+});
+$("#IncreaseVolume").click(function () {
+    cconstellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", {"plus"});
+});
+});
+$("#DecreaseVolume").click(function () {
+    cconstellation.server.sendMessage({ Scope: "Package", Args: ["RemoteControl"] }, "SetVolume", {"moins"});
+});
+});
