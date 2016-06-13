f499ee8ca1b006e26a519155530c0554796bd3f1
flo <docte@MSI-FLO>
2016-06-13 11:25:43 +0200

    Test nircmd

--- RemoteControl/RemoteControl/Program.cs
+++ RemoteControl/RemoteControl/Program.cs
@@ -2,8 +2,12 @@
 using Constellation.Package;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+
+
+
 
 namespace RemoteControl
 {
@@ -12,12 +16,16 @@ namespace RemoteControl
         static void Main(string[] args)
         {
             PackageHost.Start<Program>(args);
+            
         }
 
         public override void OnStart()
         {
             PackageHost.WriteInfo("Package starting - IsRunning : {0} - IsConnected : {1}", PackageHost.IsRunning, PackageHost.IsConnected);
             PackageHost.WriteInfo("Les Doges c'est trop bien.");
+            
         }
+        
+
     }
 }
---
+++ RemoteControl/RemoteControl/Volume.cs
@@ -0,0 +1,24 @@
+﻿using Constellation;
+using Constellation.Package;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace RemoteControl
+{
+    public class Volume : Program
+    {
+        public void SetVolume(int valeur)
+        {
+            double volume = valeur * 65.535;
+            Process nircmd = new Process();
+            nircmd.StartInfo.FileName = @"d:\nircmd\nircmd.exe";
+            nircmd.StartInfo.Arguments = "sendkeypress 0xB3";
+            nircmd.Start();
+        }
+    }
+    
+
+}
