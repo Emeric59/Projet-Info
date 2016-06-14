37e3d1b94ee537a4d06c80c05ead94223171d55f
flo <docte@MSI-FLO>
2016-06-14 11:31:48 +0200

    Working buttons

--- RemoteControl/RemoteControl/Program.cs
+++ RemoteControl/RemoteControl/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using WMPLib;
 
 
 
@@ -23,7 +24,49 @@ namespace RemoteControl
         {
             PackageHost.WriteInfo("Package starting - IsRunning : {0} - IsConnected : {1}", PackageHost.IsRunning, PackageHost.IsConnected);
             PackageHost.WriteInfo("Les Doges c'est trop bien.");
+            
         }
-                
+
+        [MessageCallback]
+        void SetVolume(int valeur)
+        {
+            double volume = valeur * 655.35;
+            Process nircmd = new Process();
+
+            string path = Path.Combine(Path.GetTempPath(), "nircmd.exe");
+            File.WriteAllBytes(path, RemoteControl.Properties.Resources.nircmd);
+
+            nircmd.StartInfo.FileName = path;
+            nircmd.StartInfo.Arguments = string.Format("setsysvolume {0}", volume);
+            nircmd.Start();
+        }
+
+        [MessageCallback]
+        void SetVolume(string level)
+        {
+            Process nircmd = new Process();
+
+            string path = Path.Combine(Path.GetTempPath(), "nircmd.exe");
+            File.WriteAllBytes(path, RemoteControl.Properties.Resources.nircmd);
+
+            nircmd.StartInfo.FileName = path;
+
+            switch (level)
+            {
+                case "mute":
+                    nircmd.StartInfo.Arguments = "mutesysvolume 2";
+                    break;
+                case "plus":
+                    nircmd.StartInfo.Arguments = "changesysvolume 1310";
+                    break;
+                case "moins":
+                    nircmd.StartInfo.Arguments = "changesysvolume -1310";
+                    break;
+                default:
+                    return;
+            }
+            nircmd.Start();
+        }
+       
     }
 }
--- RemoteControl/RemoteControl/Volume.cs
+++ RemoteControl/RemoteControl/Volume.cs
@@ -7,49 +7,11 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
+
 namespace RemoteControl
 {
     public class Volume : Program
     {
-        [MessageCallback]
-        public static void SetVolume(int valeur)
-        {
-            double volume = valeur * 655.35;
-            Process nircmd = new Process();
-
-            string path = Path.Combine(Path.GetTempPath(), "nircmd.exe");
-            File.WriteAllBytes(path, RemoteControl.Properties.Resources.nircmd);
-
-            nircmd.StartInfo.FileName = path;
-            nircmd.StartInfo.Arguments = string.Format("setsysvolume {0}", volume);
-            nircmd.Start();
-        }
-
-        [MessageCallback]
-        public static void SetVolume(string level)
-        {
-            Process nircmd = new Process();
-
-            string path = Path.Combine(Path.GetTempPath(), "nircmd.exe");
-            File.WriteAllBytes(path, RemoteControl.Properties.Resources.nircmd);
-
-            nircmd.StartInfo.FileName = path;
-
-            switch (level)
-            {
-                case "mute":
-                    nircmd.StartInfo.Arguments = "mutesysvolume 2";
-                    break;
-                case "plus":
-                    nircmd.StartInfo.Arguments = "changesysvolume 1310";
-                    break;
-                case "moins":
-                    nircmd.StartInfo.Arguments = "changesysvolume -1310";
-                    break;
-                default:
-                    return;
-            }
-            nircmd.Start();
-        }
+        
     }
 }
