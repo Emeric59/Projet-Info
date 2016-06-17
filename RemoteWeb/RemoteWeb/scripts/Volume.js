9655d068f1e978c586ebfb91c83da88becfac1fe
flo <docte@MSI-FLO>
2016-06-17 11:18:05 +0200

    essais choix album playlist

--- RemoteWeb/RemoteWeb/scripts/Volume.js
+++ RemoteWeb/RemoteWeb/scripts/Volume.js
@@ -1,11 +1,11 @@
-﻿var constellation = $.signalR.createConstellationConsumer("http://localhost:8088", "6d540ec121c933fe48ea0ad3872d5b98dec65226", "RemoteControl")
+﻿var constellation = $.signalR.createConstellationConsumer("http://localhost:8088", "615bd655bc724bc2c8eccf001f0aaf7df557849b", "RemoteControl")
 var tableau = document.getElementById("CurrentPlaylist");
 
 
 constellation.connection.stateChanged(function (change) {
     if (change.newState === $.signalR.connectionState.connected) {
         $("#state").text("Connecté")
-        constellation.server.requestSubscribeStateObjects("PCDEPIERRE_UI", "MediaPlayer", "*", "*");
+        constellation.server.requestSubscribeStateObjects("MSI-FLO_UI", "MediaPlayer", "*", "*");
     } else {
         $("#state").text("Non connecté")
     }
@@ -24,15 +24,22 @@ constellation.client.onUpdateStateObject(function (stateobject) {
         }
         for (var i = 1; i < listSize; i++) {
             var ligne = tableau.insertRow(-1);
+            //ligne.addEventListener('click', function (stateobject) {
+
+            //});
             var colonne1 = ligne.insertCell(0);
             colonne1.innerHTML += stateobject.Value[i].Item3;
+            
             var colonne2 = ligne.insertCell(1);
             colonne2.innerHTML += stateobject.Value[i].Item2;
+            colonne2.onclick = loadAlbumFromList(stateobject, i);
         }
     }
 });
 
-
+function loadAlbumFromList(stateobject, i) {
+    constellation.server.sendMessage({ Scope: "Package", Args: ["MediaPlayer"] }, "loadAlbum", stateobject.Value[i].Item2)
+};
     
 $("#SearchArtist").click(function () {
     constellation.server.sendMessage({ Scope: "Package", Args: ["MediaPlayer"]}, "loadArtist", document.getElementById("search").value)
