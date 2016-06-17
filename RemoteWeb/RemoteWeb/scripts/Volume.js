59238f05677982bef161d1ffaf090e967ccdecc6
Pierre <Pierre@PCDEPIERRE>
2016-06-17 14:55:18 +0200

    n

--- RemoteWeb/RemoteWeb/scripts/Volume.js
+++ RemoteWeb/RemoteWeb/scripts/Volume.js
@@ -1,11 +1,11 @@
-﻿var constellation = $.signalR.createConstellationConsumer("http://localhost:8088", "615bd655bc724bc2c8eccf001f0aaf7df557849b", "RemoteControl")
+﻿var constellation = $.signalR.createConstellationConsumer("http://localhost:8088", "6d540ec121c933fe48ea0ad3872d5b98dec65226", "RemoteControl")
 var tableau = document.getElementById("CurrentPlaylist");
 
 
 constellation.connection.stateChanged(function (change) {
     if (change.newState === $.signalR.connectionState.connected) {
         $("#state").text("Connecté")
-        constellation.server.requestSubscribeStateObjects("MSI-FLO_UI", "MediaPlayer", "*", "*");
+        constellation.server.requestSubscribeStateObjects("PCDEPIERRE_UI", "MediaPlayer", "*", "*");
     } else {
         $("#state").text("Non connecté")
     }
