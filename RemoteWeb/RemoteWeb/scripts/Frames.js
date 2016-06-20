d0bcd1ec746bef62bb65c9cfcda91e7b01928ad4
PraZ <PraZ@DESKTOP-6ECM952>
2016-06-21 00:04:36 +0200

    Lots of changes and additions / Frames

---
+++ RemoteWeb/RemoteWeb/scripts/Frames.js
@@ -0,0 +1,11 @@
+﻿function openMediaPlayer() {
+    document.getElementById('mainFrame').src = "MediaPlayerFrame.html";
+}
+
+function openControl() {
+    document.getElementById('mainFrame').src = "ControlFrame.html";
+}
+
+function openVolume() {
+    document.getElementById('mainFrame').src = "VolumeFrame.html";
+}
