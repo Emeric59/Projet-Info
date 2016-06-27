bd050dc2308983f68d7eb6ba0099c33c1b7c122f
Emeric <Emeric@PC-EMERIC>
2016-06-27 14:12:45 +0200

    Correction

--- RemoteControl/RemoteControl/Program.cs
+++ RemoteControl/RemoteControl/Program.cs
@@ -268,7 +268,7 @@ namespace RemoteControl
             string path = Path.Combine(Path.GetTempPath(), "nircmd.exe");
             File.WriteAllBytes(path, RemoteControl.Properties.Resources.nircmd);
 
-            DialogResult dialogResult = MessageBox.Show("Voulez-vous vraiment rédemarrer l'ordinateur ?", "Reboot ?", MessageBoxButtons.YesNo);
+            DialogResult dialogResult = MessageBox.Show("Voulez-vous vraiment redémarrer l'ordinateur ?", "Reboot ?", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
             {
                 nircmd.StartInfo.FileName = path;
